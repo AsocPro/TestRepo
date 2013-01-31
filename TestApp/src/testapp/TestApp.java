972096184b5b06bb1fb35ef104bb35a45b27ed2e
Zachary Pratt Gibbs <Zachary Pratt Gibbs@MasterControl>
2013-01-30 23:35:39 -0700

    Should be the second Commit

--- TestApp/src/testapp/TestApp.java
+++ TestApp/src/testapp/TestApp.java
@@ -1,6 +1,6 @@
 /*
  * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * and open 
  */
 package testapp;
 
@@ -14,6 +14,9 @@ public class TestApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here 
+        //so what happens when I add to something here
+        
+        //or take away some comments from above.
     }
 }
